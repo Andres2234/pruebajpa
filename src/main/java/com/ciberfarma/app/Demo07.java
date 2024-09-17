07d2c2d0494a7375c4479294af0590e79b2d5b13
unknown <SUITE@SUITECE205-ST24.cibertec.edu.pe>
2024-09-17 18:13:19 -0500

    Proyecto Mantenimiento de Productos

---
+++ src/main/java/com/ciberfarma/app/Demo07.java
@@ -0,0 +1,35 @@
+package com.ciberfarma.app;
+
+import java.util.List;
+
+import com.ciberfarma.model.Tipo;
+import com.ciberfarma.model.Usuario;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Demo07 {
+	public static void main(String[]args) {
+		EntityManagerFactory conexion = Persistence.createEntityManagerFactory("jpa_sesion01");
+		EntityManager em = conexion.createEntityManager();
+		
+		
+		// CASO CUANDO SE USA  SQL
+		List<Tipo>lstTipos= em.createNativeQuery("select * from tb_tipos",Tipo.class).getResultList();
+		
+		for (Tipo t : lstTipos) {
+			System.out.println("Tipo de Usuario: " + t.getDescripcion());
+			System.out.println("-------------------------------");
+			for (Usuario u : t.getLstUsuarios()) {
+				System.out.println("Nombre: " + u.getNom_usua() + " " + u.getApe_usua());
+				System.out.println("Email : " + u.getCorreo());
+				System.out.println("--------------------------");
+			}
+		}
+		
+		// CASO CUANDO SE USA  JPQL
+		List<Tipo>lstTipos2= em.createQuery("select u from Tipo u").getResultList();
+		
+	}
+}
